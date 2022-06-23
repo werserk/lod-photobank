b85466cdaf2bcc67125387870726d47f3adc133b
werserk <werserk@inbox.ru>
2022-06-23 23:02:55 +0300

    добвалено поле ввода, экспандеры

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from utils import read_files, create_folder
 from production import get_setup
+import cv2
 
 
 @st.cache
@@ -10,6 +11,7 @@ def cached_get_setup():
 
 def main():
     models, transforms = cached_get_setup()
+
     page_bg_img = '''
     <style>
     .stApp {
@@ -21,16 +23,18 @@ def main():
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-    for folder in ['segmentations/', 'images/']:
-        create_folder(folder)
-
     st.title('Фотобанк')
+    st.text_input('Запрос по архиву', value="")
 
-    st.subheader("Загрузка файлов")
-    filenames = st.file_uploader('Выберите или ператащите сюда снимки', type=['png', 'jpeg', 'jpg'],
-                                 accept_multiple_files=True)
+    list_of_files = st.expander('Найденные файлы')
+    for i in range(3):
+        img = cv2.cvtColor(cv2.imread('image.jpg'), cv2.COLOR_BGR2RGB)
+        list_of_files.image(img)
 
-    if st.button('Загрузить') and filenames:
+    download_expender = st.expander('Загрузка файлов')
+    filenames = download_expender.file_uploader('Выберите или ператащите сюда снимки', type=['png', 'jpeg', 'jpg'],
+                                                accept_multiple_files=True)
+    if download_expender.button('Загрузить') and filenames:
         paths, folder_name = read_files(filenames)
         if not paths:
             st.error('Неправильный формат или название файла')
