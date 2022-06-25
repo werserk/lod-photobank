61377f0cd86887f0c2512d9729b171c8f670c8a0
werserk <werserk@inbox.ru>
2022-06-25 15:22:38 +0300

    search by text (not done)

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import streamlit as st
 
-from app.extract.sources import LocalSource
 import production
-from utils import read_files
-import cv2
+import utils
+
+tags = 'Время суток	Время года	Местность	Авиа	Автомобили	БПЛА	Водолаз	Кинолог	Кони	Объятия	Шерп'.split(
+    '\t')
 
 
-# import hydra
-# from omegaconf import DictConfig
-
-
-# @st.cache
 def cached_get_setup():
     return production.get_setup()
 
 
-# @hydra.main(version_base="1.1", config_path="conf", config_name="default")
-# def main(config: DictConfig):
 def main():
     model, processor = cached_get_setup()
 
@@ -30,27 +24,32 @@ def main():
     '''
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
-
     st.title('Фотобанк')
-    st.text_input('Запрос по архиву', value="")
 
-    list_of_files = st.expander('Найденные файлы')
-    # for i in range(3):
-    #     img = cv2.cvtColor(cv2.imread(f"{config.root_path}/image.jpg"), cv2.COLOR_BGR2RGB)
-    #     list_of_files.image(img)
+    ### SIDEBAR
+    for tag in tags:
+        st.sidebar.checkbox(tag)
 
+    ### БЛОК ЗАГРУЗКИ ФАЙЛОВ
     download_expender = st.expander('Загрузка файлов')
     filenames = download_expender.file_uploader('Выберите или ператащите сюда снимки', type=['png', 'jpeg', 'jpg'],
                                                 accept_multiple_files=True)
-    # local_loader = LocalSource(config)
     if download_expender.button('Загрузить') and filenames:
-        # paths = local_loader.extract_files(filenames)
-        paths = read_files(filenames)
+        paths = utils.read_files(filenames)
         if not paths:
             st.error('Неправильный формат или название файла')
         else:
             embeddings = production.get_embeddings(model, processor, paths)
             production.save_embeddings(embeddings)
+
+    ### ОСНОВНАЯ ЛЕНТА
+    request = st.text_input('Поиск по описанию', value="")
+    st.button('Искать')
+    if st.button:
+        data = production.load_db_embeddings()
+        embeddings = production.get_embeddings_from_text(processor, request)
+        max_k = production.search_max_similary(data, embeddings)
+        print(max_k)
 
 
 if __name__ == '__main__':
--- production.py
+++ production.py
@@ -2,11 +2,15 @@ import ruclip
 import torch
 from PIL import Image
 import os
+import faiss
 import pandas as pd
 import string
 import random
+import numpy as np
 
 DEVICE = 'cpu'
+EMBEDDING_LENGTH = 768  # длина эмбеддинга
+K = 5
 
 
 def get_setup():
@@ -55,3 +59,27 @@ def save_embeddings(embeddings):
                       columns=["path"] + [f"embeddins_{i}" for i in range(embeddings[0][1].shape[-1])])
     filename = generate_filename()
     df.to_feather(f"data/embeddings/{filename}.feather")
+
+
+def load_db_embeddings():
+    folder = 'data/embeddings'
+    feathers = os.listdir(folder)
+    df_res = pd.read_feather(os.path.join(folder, feathers[0]))
+    for feather in feathers[1:]:
+        df = pd.read_feather(os.path.join(folder, feather))
+        df_res.append(df)
+    return df_res
+
+
+def search_max_similary(data, embeddings):
+    index = faiss.IndexFlatL2(EMBEDDING_LENGTH)
+    index.add(embeddings)
+    data = data.iloc[:, 1:].to_numpy()
+    print(data.shape)
+    D, I = index.search(data, K)
+    return data.iloc[I, 0]
+
+
+def get_embeddings_from_text(processor, request):
+    embeddings = processor(text=request, return_tensors='pt', padding=True)["input_ids"]
+    return embeddings
