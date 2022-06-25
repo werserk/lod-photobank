38a2c5f625e82b2c3455e24b35ccb6c0f7cf12e9
werserk <werserk@inbox.ru>
2022-06-25 14:03:20 +0300

    saving embeddings and images (done)

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import streamlit as st
 
 from app.extract.sources import LocalSource
-from production import get_setup
+import production
+from utils import read_files
 import cv2
 
-import hydra
-from omegaconf import DictConfig
+
+# import hydra
+# from omegaconf import DictConfig
 
 
-@st.cache
+# @st.cache
 def cached_get_setup():
-    return get_setup()
+    return production.get_setup()
 
 
-@hydra.main(version_base="1.1", config_path="conf", config_name="default")
-def main(config: DictConfig):
-    models, processor = cached_get_setup()
+# @hydra.main(version_base="1.1", config_path="conf", config_name="default")
+# def main(config: DictConfig):
+def main():
+    model, processor = cached_get_setup()
 
     page_bg_img = '''
     <style>
@@ -32,18 +35,22 @@ def main(config: DictConfig):
     st.text_input('Запрос по архиву', value="")
 
     list_of_files = st.expander('Найденные файлы')
-    for i in range(3):
-        img = cv2.cvtColor(cv2.imread(f"{config.root_path}/image.jpg"), cv2.COLOR_BGR2RGB)
-        list_of_files.image(img)
+    # for i in range(3):
+    #     img = cv2.cvtColor(cv2.imread(f"{config.root_path}/image.jpg"), cv2.COLOR_BGR2RGB)
+    #     list_of_files.image(img)
 
     download_expender = st.expander('Загрузка файлов')
     filenames = download_expender.file_uploader('Выберите или ператащите сюда снимки', type=['png', 'jpeg', 'jpg'],
                                                 accept_multiple_files=True)
-    local_loader = LocalSource(config)
+    # local_loader = LocalSource(config)
     if download_expender.button('Загрузить') and filenames:
-        paths = local_loader.extract_files(filenames)
+        # paths = local_loader.extract_files(filenames)
+        paths = read_files(filenames)
         if not paths:
             st.error('Неправильный формат или название файла')
+        else:
+            embeddings = production.get_embeddings(model, processor, paths)
+            production.save_embeddings(embeddings)
 
 
 if __name__ == '__main__':
--- production.py
+++ production.py
@@ -2,6 +2,9 @@ import ruclip
 import torch
 from PIL import Image
 import os
+import pandas as pd
+import string
+import random
 
 DEVICE = 'cpu'
 
@@ -18,30 +21,37 @@ def gen_batch(inputs, batch_size):
         batch_start += batch_size
 
 
-def get_embeddings(model, processor, paths):
+def get_embeddings(model, processor, data):
     results = []
+    data.sort()
     batch_size = 128
-
-    with torch.no_grad():
-        for path in paths:
-            data = [os.path.join(path, filename)
-                    for filename in os.listdir(path)]
-            data.sort()
-            batches = list(gen_batch(inputs=data, batch_size=batch_size))
-            for batch in batches:
-                pil_images = []
-                for idx, path2img in enumerate(batch):
-                    try:
-                        image = Image.open(path2img)
-                        pil_images.append(image)
-                    except:
-                        del batch[idx]
-                batch_torch = processor(images=pil_images,
-                                        return_tensors='pt',
-                                        padding=True)["pixel_values"].to(DEVICE)
-                preds = model.encode_image(batch_torch)
-                for idx, (path2img, embeddings) in enumerate(zip(batch, preds)):
-                    results.append([path2img, embeddings])
+    batches = list(gen_batch(inputs=data, batch_size=batch_size))
+    for batch in batches:
+        pil_images = []
+        for idx, path2img in enumerate(batch):
+            try:
+                image = Image.open(path2img)
+                pil_images.append(image)
+            except:
+                del batch[idx]
+        with torch.no_grad():
+            batch_torch = processor(images=pil_images,
+                                    return_tensors='pt',
+                                    padding=True)["pixel_values"].to(DEVICE)
+            preds = model.encode_image(batch_torch)
+        for idx, (path2img, embeddings) in enumerate(zip(batch, preds)):
+            results.append([path2img, embeddings])
     return results
 
 
+def generate_filename():
+    filename = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+    return filename
+
+
+def save_embeddings(embeddings):
+    embeddings = [(i, j.detach().cpu().numpy()) for i, j in embeddings]
+    df = pd.DataFrame([(i, *j) for i, j in embeddings],
+                      columns=["path"] + [f"embeddins_{i}" for i in range(embeddings[0][1].shape[-1])])
+    filename = generate_filename()
+    df.to_feather(f"data/embeddings/{filename}.feather")
